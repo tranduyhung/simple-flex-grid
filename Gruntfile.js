86c640851f8fe183865d8d8eee199b6253593b23
Hung Tran <nguyentieuhau@gmail.com>
2022-06-24 16:52:04 +0700

    Init commit

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+  const sass = require('node-sass');
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    config: {
+      srcFolder: 'src',
+      distFolder: 'dist'
+    },
+
+    browserSync: {
+      bsFiles: {
+        src : [
+          '<%= config.distFolder %>/'
+        ],
+      },
+      options: {
+        server: '<%= config.distFolder %>/'
+      }
+    },
+
+    watch: {
+      scss: {
+        expand: true,
+        files: ['<%= config.srcFolder %>/*.scss'],
+        tasks: ['sass', 'cssmin']
+      }
+    },
+
+    sass: {
+      app: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.srcFolder %>',
+          src: ['**/*.scss'],
+          dest: '<%= config.distFolder %>',
+          ext: '.css'
+        }],
+        options: {
+          implementation: sass,
+          sourceMap: true,
+        }
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.distFolder %>',
+          src: ['*.css', '!*.min.css'],
+          dest: '<%= config.distFolder %>',
+          ext: '.min.css'
+        }]
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['watch', 'browserSync']);
+  grunt.registerTask('build', ['sass', 'cssmin']);
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple Flex Grid #
+
+A crazy simple responsive flexbox grid. This grid is used as a skeleton to customize and use together with other CSS grid systems, it is not ready for production.
+
+## License
+
+This grid is licensed under the [MIT license](LICENSE.md)
